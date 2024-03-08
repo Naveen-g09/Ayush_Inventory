8747df7f427e45a623b4e5a4a2e4d4bc793b6bdf
Naveen <naveengummella.rg@gmail.com>
2024-03-08 21:58:10 +0530

    adding eeslint

--- app/lessStock.tsx
+++ app/lessStock.tsx
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text } from "react-native";
 
 const lessStock = () => {
   return (
     <View>
       <Text>lessStock</Text>
     </View>
-  )
-}
+  );
+};
 
-export default lessStock
+export default lessStock;
