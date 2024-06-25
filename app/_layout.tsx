662aa4de3bc96d4eeed9019cccfab3081bf190a3
Naveen-g09 <naveengummella.rg@gmail.com>
2024-06-26 02:45:12 +0530

    clean

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,6 @@ import React, { useEffect } from "react";
 import { Pressable } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import { useColorScheme } from "@/components/useColorScheme";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -48,85 +47,82 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider
-      value={colorScheme === "light" ? DefaultTheme : DefaultTheme}
-    >
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <Drawer
-          screenOptions={{
-            // Disable the static render of the header on web
-            // to prevent a hydration error in React Navigation v6.
-            // headerShown: useClientOnlyValue(false, true),
-            // headerTitle: "Ayush Inventory",
-            headerStyle: {
-              backgroundColor: "lavender",
-              elevation: 4,
-            },
-            headerTintColor: "black",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-            headerTitleAlign: "center",
-            headerPressColor: "rgba(0, 0, 0, .32)",
-            headerRight: () => (
-              <Link href="/notifications" asChild>
-                <Pressable>
-                  {({ pressed }) => (
-                    <FontAwesome
-                      name="bell-o"
-                      size={25}
-                      style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                    />
-                  )}
-                </Pressable>
-              </Link>
-            ),
+
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer
+        screenOptions={{
+          // Disable the static render of the header on web
+          // to prevent a hydration error in React Navigation v6.
+          // headerShown: useClientOnlyValue(false, true),
+          // headerTitle: "Ayush Inventory",
+          headerStyle: {
+            backgroundColor: "lavender",
+            elevation: 4,
+          },
+          headerTintColor: "black",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerTitleAlign: "center",
+          headerPressColor: "rgba(0, 0, 0, .32)",
+          headerRight: () => (
+            <Link href="/notifications" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="bell-o"
+                    size={25}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      >
+        <Drawer.Screen
+          name="index" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: "Inventory",
+            title: "Ayush Inventory",
           }}
-        >
-          <Drawer.Screen
-            name="index" // This is the name of the page and must match the url from root
-            options={{
-              drawerLabel: "Inventory",
-              title: "Ayush Inventory",
-            }}
-          />
+        />
 
-          <Drawer.Screen
-            name="contacts"
-            options={{
-              drawerLabel: "Contacts",
-              title: "Contacts",
-            }}
-          />
+        <Drawer.Screen
+          name="contacts"
+          options={{
+            drawerLabel: "Contacts",
+            title: "Contacts",
+          }}
+        />
 
-          <Drawer.Screen
-            name="lessStock"
-            options={{
-              drawerLabel: "Minimal Stock",
-              title: "Minimal Stock",
-            }}
-          />
+        <Drawer.Screen
+          name="lessStock"
+          options={{
+            drawerLabel: "Minimal Stock",
+            title: "Minimal Stock",
+          }}
+        />
 
-          <Drawer.Screen
-            name="notifications"
-            options={{
-              drawerLabel: "Notifications",
-              title: "Notifications",
-              drawerItemStyle: { display: "none" },
-            }}
-          />
+        <Drawer.Screen
+          name="notifications"
+          options={{
+            drawerLabel: "Notifications",
+            title: "Notifications",
+            drawerItemStyle: { display: "none" },
+          }}
+        />
 
-          <Drawer.Screen
-            name="+not-found"
-            options={{
-              drawerItemStyle: { display: "none" },
-            }}
-          />
-        </Drawer>
-      </GestureHandlerRootView>
-    </ThemeProvider>
+        <Drawer.Screen
+          name="+not-found"
+          options={{
+            drawerItemStyle: { display: "none" },
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
+
   );
 }
